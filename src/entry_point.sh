95d09672d1980bd190cf8de0fee3aab12afb6cc5
vasu-sachdeva-31 <127660164+vasu-sachdeva-31@users.noreply.github.com>
2024-05-03 01:15:57 +0530

    Add files via upload

---
+++ src/entry_point.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python manage.py makemigrations
+python manage.py migrate
+python manage.py migrate --fake sessions zero
+python manage.py migrate --fake-initial
+python manage.py shell < init_admin.py
+python manage.py makemigrations app
+python manage.py migrate app
+echo "Django is ready.";
+python manage.py runserver 0.0.0.0:8000
